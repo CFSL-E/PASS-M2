e1c7dbd97b8736586f3a4a7258ac70d887e5972c
CFSL-E <ac1.dl.cloudflare.com.ser@ad.elmail.cloudns.ch>
2026-05-18 00:18:22 +0800

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
     }
 }
 
